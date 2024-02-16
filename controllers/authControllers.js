f1b09efad2af94dc6999eff7ae2dc5be0075c618
Yashvimangroliya <yashvimangroliya@gmail.com>
2024-02-16 17:05:11 -0500

    add login data

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -36,6 +36,58 @@ const registerController = async (req, res) => {
 
 //login callback
 
-const loginController = () => {};
+const loginController = async(req,res) => {
+  try {
+    const user = await dataModel.findOne({ email: req.body.email });
+    if(!user){
+      return res.status(404).send({
+        success:false,
+        message:"invalid credentials"
+      })
+    }
+    //compare password
+    const comparePassword = await bcrypt.compare(req.body.password, user.password)
+    if(!comparePassword){
+      return res.status(500).send({
+        success:false,
+        message:"invalid credentials"
+      })
+    }
+    const token = jwt.sign({userId:user._id},process.env.JWT_SECRET,{expiresIn:'1d'})
+    return res.status(200).send({
+      success:true,
+      message:"Login Successfully",
+      token,
+      user,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      success:false,
+      message:'Error in login API',
+      error
+    })
+  }
+};
 
-module.exports = { registerController, loginController };
+//GET CURRENT USER
+const currentUserController = async (req, res) => {
+  try {
+    const user = await dataModel.findOne({_id:req.body.userId})
+    return res.status(200).send({
+      success:true,
+      message:"user fetched successfully",
+      user
+    })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).send({
+      success:false,
+      message:"unable to get current user",
+      error
+    })
+  }
+}
+
+
+module.exports = { registerController, loginController, currentUserController };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
 const express =require('express')
-const { registerController, loginController } = require('../controllers/authControllers')
+const { registerController, loginController, currentUserController } = require('../controllers/authControllers');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router()
-
+//Register
 router.post("/register",registerController);
+//Login 
+router.post("/login", loginController);
+//GET Current user
+router.get("/current-user", authMiddleware, currentUserController)
 
-router.post("/login", loginController)
+module.exports = router;
 
-module.exports = router
